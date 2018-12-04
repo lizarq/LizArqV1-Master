c8f3c9d66ec5d508cd4067445622c43c2bb68e3b
lizarq <yaninaglaser@gmail.com>
2018-12-04 09:06:49 -0300

    funciona el close

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  cancel(){
+    this.cancelForm();
+  }
+  cancelForm(){
+    console.log("cancel")
+    this.router.navigate(['../Portfolio'])
+  }
 }
